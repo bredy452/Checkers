edc9109c2b3644f00c9f94a200a73c9906a9106e
Bredell_Evans <bredy452@gmail.com>
2021-02-10 18:03:02 -0500

    got pieces to append only to red squares

--- js/app.js
+++ js/app.js
@@ -31,28 +31,36 @@ $(() => {
 		$div.appendTo($('.checkerboardContainer'))
 		//give spaces checkerboard colors
 		if ($h1.text() % 2 === 0 && $h1.text() <= 8) {
-			$div.css('background-color', '#DB1F2A').attr('id', 'droppable')
+			$div.css('background-color', '#DB1F2A')
+			// .attr('id', 'droppable')
 			$div.droppable()
 		} else if ($h1.text() % 2 === 1 && $h1.text() <= 16 && $h1.text() >= 9) {
-			$div.css('background-color', '#DB1F2A').attr('id', 'droppable')
+			$div.css('background-color', '#DB1F2A')
+			// .attr('id', 'droppable')
 			$div.droppable()
 		} else if ($h1.text() % 2 === 0 && $h1.text() <= 24 && $h1.text() >= 17){
-			$div.css('background-color', '#DB1F2A').attr('id', 'droppable')
+			$div.css('background-color', '#DB1F2A')
+			// .attr('id', 'droppable')
 			$div.droppable()
 		} else if ($h1.text() % 2 === 1 && $h1.text() <= 32 && $h1.text() >= 25) {
-			$div.css('background-color', '#DB1F2A').attr('id', 'droppable')
+			$div.css('background-color', '#DB1F2A')
+			// .attr('id', 'droppable')
 			$div.droppable()
 		} else if ($h1.text() % 2 === 0 && $h1.text() <= 40 && $h1.text() >= 33){
-			$div.css('background-color', '#DB1F2A').attr('id', 'droppable')
+			$div.css('background-color', '#DB1F2A')
+			// .attr('id', 'droppable')
 			$div.droppable()
 		} else if ($h1.text() % 2 === 1 && $h1.text() <= 48 && $h1.text() >= 41){
-			$div.css('background-color', '#DB1F2A').attr('id', 'droppable')
+			$div.css('background-color', '#DB1F2A')
+			// .attr('id', 'droppable')
 			$div.droppable()
 		} else if ($h1.text() % 2 === 0 && $h1.text() <= 56 && $h1.text() >= 49) {
-			$div.css('background-color', '#DB1F2A').attr('id', 'droppable')
+			$div.css('background-color', '#DB1F2A')
+			// .attr('id', 'droppable')
 			$div.droppable()
 		} else if ($h1.text() % 2 === 1 && $h1.text() <= 64 && $h1.text() >= 57){
-			$div.css('background-color', '#DB1F2A').attr('id', 'droppable')
+			$div.css('background-color', '#DB1F2A')
+			// .attr('id', 'droppable')
 			$div.droppable()
 		} else {
 			$div.css('background-color', '#221A03')
@@ -69,7 +77,7 @@ $(() => {
 	 			const $div = $('<div>').attr('class', `${pieceColor}Pieces`).attr('class', 'pieces').css('height', '50px').css('width', '50px	').css('border-radius', '50%').css('background-color', `${pieceColor}`)
 	 			const $h2 = $('<h2>').text(i).css('color', 'white')
 	 			$div.append($h2)
-	 			$div.attr('id', 'draggable')
+	 			// $div.attr('id', 'draggable')
 	 			$div.draggable()
 	 			$div.appendTo($(`.${pieceColor}CheckerPieceContainer`))
 	 			if (pieceColor === 'red') {
@@ -106,10 +114,34 @@ $(() => {
 		 }
 	}
 
-	const $draggable = () => {
-		$('#draggable').draggable()
-	 //event for when I drop the pieces for player 1
-	}
+
+
+
+
+
+
+
+
+
+	// const $draggable = () => {
+	// 	const $drag = $('#draggable').draggable({appendTo: $('#draggable')})
+
+	 	const $movement = () => {
+			$('.ui-draggable').draggable({
+				helper:'clone',
+				// containment: '.ui-droppable'
+			})
+			$('.ui-droppable').droppable({
+				drop: function(event, ui) {
+					ui.draggable.detach().appendTo($(this))
+					console.log(ui.draggable)
+				}
+			})
+		}
+	// 	}
+	// }
+
+	
 
 	// $('#draggable').draggable()
 	// const $dropPieces = () => {
@@ -133,7 +165,7 @@ $(() => {
 	$makingPieces('black')
 	$makingPieces('red')
 	$newGame()
-	$draggable()
+	$movement()
 
 
 
